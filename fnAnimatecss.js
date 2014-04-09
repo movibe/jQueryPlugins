0178d036f99695f52c9fce5c75ea6c53a448f4b1
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-04-09 15:45:03 -0300

    animateCSS: Documentação e melhorias

--- fnAnimatecss.js
+++ fnAnimatecss.js
@@ -1,9 +1,27 @@
 ( function ( window, $, undefined ) {
+	/**
+	 * animateCSS
+	 *
+	 * @author Willian Ribeiro Angelo <willian.ribeiro.angelo@gmail.com>
+	 * @package jQueryPlugin
+	 *
+	 *
+	 * Adicione o Animate CSS:
+	 * http://daneden.github.io/animate.css/
+	 *
+	 * Adicione este Arquivo no seu Projeto
+	 *
+	 * Como Usar:
+	 * $(obj).animateCSS('pulse');
+	 *
+	 */
+
+
 	//Versão
 	var VERSION = '1.0';
 
 	// Funções
-	$.fn.animar = function ( animacao ) {
+	$.fn.animateCSS = function ( animacao ) {
 		$( this ).addClass( animacao + ' animated' ).one( 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
 			$( this ).removeClass( animacao + ' animated' );
 			$( this ).focus();
